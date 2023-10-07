8810316b9ddeb9ddc4a99f7ac2db57161970122a
light23-i <74209656+light23-i@users.noreply.github.com>
2023-10-07 13:14:41 +0530

    Update llama2test.js

--- backend/llama2test.js
+++ backend/llama2test.js
@@ -3,7 +3,7 @@ import readline from 'readline';
 import { LLM } from 'llama-node';
 import { RetrievalQAChain } from 'langchain/chains';
 import { LLamaCpp } from 'llama-node/dist/llm/llama-cpp.js';
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+
 import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { LLamaEmbeddings } from 'llama-node/dist/extensions/langchain.js';
@@ -25,21 +25,15 @@ const config = {
 
 const llama = new LLM(LLamaCpp);
 
-const loader = new DirectoryLoader('sources', {
-  '.pdf': (path) => new PDFLoader(path, { splitPages: false })
-});
 
 llama.load(config).then(handler);
 
 async function handler() {
   const data = await loader.load();
   const textSplitter = new RecursiveCharacterTextSplitter();
-  const splitDocs = await textSplitter.splitDocuments(data);
   const embeddings = new LLamaEmbeddings({ maxConcurrency: 1 }, llama);
   const vectorStore = await MemoryVectorStore.fromDocuments(splitDocs, embeddings);
 
-  const template = `Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Keep the answer as concise as possible.
-{context}
 Question: {question}
 Helpful Answer:`;
 
